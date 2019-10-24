aa4478247473a29a940595d5dab8cbb1200c9afb
minematsushun <50306222+minematsushun@users.noreply.github.com>
2019-10-24 18:25:56 +0900

    自動更新機能の追加

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,8 @@ $(function(){
 
   function buildHTML(message){
     let addImage = (message.image !== null)? `<img class="lower-message__image" src="${message.image}">`:''
-    let html = `<div class="main__content__message">
+
+    let html = `<div class="main__content__message" data-message-id="${message.id}">
                   <div class="main__content__message__upper-info">
                     <div class="main__content__message__upper-info__name">
                       ${message.user}
@@ -43,8 +44,33 @@ $(function(){
     })
     .always(function(data){
       $('.form__submit').prop('disabled',false);
-    })
+    });
   })
-})
+
+  var reloadMessages = function(){
+    var last_message_id = $(".main__content__message:last").data("message-id")
+    $.ajax({
+      url: "api/messages",
+      type: "GET",
+      dataType: "json",
+      data: {id: last_message_id}
+    })
+    .done(function(messages){
+        let insertHTML = "";
+        messages.forEach(function(message){
+          insertHTML = buildHTML(message);
+          $(".main__content").append(insertHTML);
+        })
+        $(".main__content").animate({scrollTop:$(".main__content")[0].scrollHeight}, "slow");
+    })
+    .fail(function(){
+      alert("自動更新失敗です")
+    });
+
+  };
+  if (window.location.href.match(/groups\/\d\/messages/)){
+    setInterval(reloadMessages, 5000);
+  }
+});
 
 
