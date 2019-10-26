1612994f1733f1af307808f79a2c3c27776ad204
minematsushun <50306222+minematsushun@users.noreply.github.com>
2019-10-26 14:37:16 +0900

    グループの機能などを更新

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -6,7 +6,7 @@ $(function(){
   function appendUser(user){
     let html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${user.name}</p>
-                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+                  <p class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</p>
                 </div>`
     search_list.append(html);
   }
@@ -16,13 +16,13 @@ $(function(){
                   <input name='group[user_ids][]' type='hidden' value='${user_id}'>
                   <p class="chat-group-user__name">${user_name}</p>
                   <a class="user-search-add chat-group-user__btn chat-group-user__btn--del" data-user-id="${user_id}" data-user-name="${user_name}">削除</a>
+                  <p class="user-search-add chat-group-user__btn chat-group-user__btn--del" data-user-id="${user_id}" data-user-name="${user_name}">削除</p>
                 </div>`
     searched_list.append(html);
   }
 
   $("#user-search-field").on("keyup",function(){
     let input = $("#user-search-field").val();
-
     $.ajax({
       type: "GET",
       url: "/users",
@@ -40,23 +40,22 @@ $(function(){
 
     .fail(function(){
       alert("ユーザー検索に失敗しました");
-    })
-
-
-    $("#user-search-result").on("click", ".chat-group-user__btn--add" , function(){
-      let user_id = $(this).data('user-id');
-      let user_name = $(this).data('user-name');
-      appendedUser(user_id, user_name);
-      $(this).parent().remove();
-    })
-
-    $("#user-add-result").on("click", ".chat-group-user__btn--del" , function(){
-      $(this).parent().remove();
-    })
+    });
   });
 
+
+  $("#user-search-result").on("click", ".chat-group-user__btn--add" , function(){
+    let user_id = $(this).data('user-id');
+    let user_name = $(this).data('user-name');
+    appendedUser(user_id, user_name);
+    $(this).parent().remove();
+  })
+
+  $("#user-add-result").on("click", ".chat-group-user__btn--del" , function(){
+    $(this).parent().remove();
+  })
+
   $(".chat-group-user__btn--del").on("click", function(){
-    console.log(this)
     $(this).parent().remove();
   });
 });
