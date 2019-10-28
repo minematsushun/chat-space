e4c412a32769e18224ab289974c6572df5504820
minematsushun <50306222+minematsushun@users.noreply.github.com>
2019-10-28 18:38:46 +0900

    delete bindingpry

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
 
   def index
     @users = User.where('name LIKE(?) and (id!=?)', "%#{params[:keyword]}%", "#{current_user.id}").where.not(id: params[:error])
-    binding.pry
     respond_to do |format|
       format.html
       format.json
